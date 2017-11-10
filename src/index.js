ce44922de6c2c2cffd9260e79dc29e04d220ce51
Jamie Ellis <Jamie.Ellis@softwire.com>
2017-11-10 15:47:34 +0000

    Setup express webserver.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+import express from 'express';
 import { TflApiClient } from "./clients/TflApiClient";
 import { PostcodesClient } from "./clients/PostcodesClient";
 
@@ -8,6 +9,13 @@ const config = require('../config.json');
 const postcodesClient = new PostcodesClient();
 const tflApiClient = new TflApiClient(config.tflCredentials.applicationId, config.tflCredentials.applicationKey);
 
-postcodesClient.getLocation('NW51TL')
-  .then(location => tflApiClient.getStopsForLocation(location, 500))
-  .then(console.log);
+const app = express();
+
+app.get('/busBoard', (request, response) =>
+  postcodesClient.getLocation(request.query.postcode)
+    .then(location => tflApiClient.getStopsForLocation(location, 500))
+    .then(stops => response.send(stops))
+    .catch(console.log)
+);
+
+app.listen(3000, () => console.log('BusBoard listening on port 3000!'));
