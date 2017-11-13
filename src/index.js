7e10f1d62091ca822d3bbe31bcec4c0473d0f3a3
Ben Ward <Ben.Ward@softwire.com>
2017-11-13 13:16:52 +0000

    Removed frontend

--- src/index.js
+++ src/index.js
@@ -11,27 +11,13 @@ const tflApiClient = new TflApiClient(config.tflCredentials.applicationId, confi
 
 const app = express();
 
-app.set('view engine', 'pug');
-
-app.get('/busBoard', async (request, response) => {
-
-  let location = await postcodesClient.getLocation(request.query.postcode);
-  let stops = await tflApiClient.getStopsForLocation(location, 500);
-  let buses = await Promise.all(stops.map(stop => tflApiClient.getBusesForStop(stop)));
-  response.send(stops.map((stop, i) => { return {stop: stop, buses: buses[i]}}));
-
-});
-
 app.get('/postcode/:postcode', async (request, response) => {
 
   let location = await postcodesClient.getLocation(request.params.postcode);
   let stops = await tflApiClient.getStopsForLocation(location, 500);
   let buses = await Promise.all(stops.map(stop => tflApiClient.getBusesForStop(stop)));
-  let data = stops.map((stop, i) => { return {stop: stop, buses: buses[i]}});
-
-  response.render('index', {data: data});
+  response.send(stops.map((stop, i) => { return {stop: stop, buses: buses[i]}}));
 });
 
-
-app.listen(3000, () => console.log('BusBoard listening on port 3000!'));
+app.listen(3001, () => console.log('BusBoard listening on port 3001!'));
 
